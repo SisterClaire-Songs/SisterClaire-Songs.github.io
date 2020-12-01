55b9216c48f26575ce7c613182f3fd249a3ddf02
SisterClaire-Songs <skm9348@naver.com>
2020-12-02 02:06:49 +0900

    one step..?

--- client/components/Sidebar/sidebar.vue
+++ client/components/Sidebar/sidebar.vue
@@ -11,12 +11,49 @@
       <el-menu-item index="/archive">Archive</el-menu-item>
       <el-menu-item index="/about">About</el-menu-item>
     </el-menu>
+
+
+1 {{ value }}<br>
+    2 {{ songdata }}<br>
+    3 {{ archive }}<br>
+
+<el-select v-model="value" placeholder="Select">
+    <el-option      
+      key="aaa"
+      label="aaa"
+      value="aaa">
+    </el-option>
+    <el-option      
+      key="bbb"
+      label="bbb"
+      value="bbb">
+    </el-option>
+  </el-select>
+  
+    
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Sidebar'
+  name: 'Sidebar',
+  data () {
+    return {
+      songdata: 'aaa1',
+      archive: 'bbb1'
+    }
+  },
+  methods: {
+    change1: function () {
+      this.songdata = 'aaa1'
+      this.archive = 'bbb1'
+    },
+    change2: function () {
+      this.songdata = 'aaa2'
+      this.archive = 'bbb2'
+    }
+  }
 }
 </script>
 
