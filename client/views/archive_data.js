48682a232613e9f965856ce6e891383d71ce0355
SisterClaire-Songs <skm9348@naver.com>
2020-12-06 03:56:11 +0900

    # ADD - Search in Archive data, and make table
    Song 페이지의 기초 기능인 archive 데이터에서 전부 검색해오는 기능을
    구현했습니다.
    이제 기초 기능은 다 되는겁니다!
    데이터를 적당히 추가한 뒤, 필터가 잘 되는지를 테스트하는 과정을 거치면
    될 것 같습니다.

--- client/views/archive_data.js
+++ client/views/archive_data.js
@@ -2,7 +2,8 @@ import SongData from './song_data.js'
 
 const ArchiveData = [
   {
-    'title': '【歌枠】元気に歌うぞ～！✨【にじさんじ/シスター・クレア】',
+    'title': ['【歌枠】元気に歌うぞ～！✨【にじさんじ/シスター・クレア】',
+      '【노래방송】기운차게 노래할게요～！✨【니지산지/시스터・클레어】'],
     'url': 'https://www.youtube.com/watch?v=TvSfQKSSr3g',
     'date': '20200424',
     'setlist': [
@@ -10,104 +11,153 @@ const ArchiveData = [
         'time_vis': '02:59',
         'time_sec': '179',
         'song': SongData.get('虹いろ・クマクマ').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '10:07',
         'time_sec': '607',
         'song': SongData.get('○○ がんばらなくてもええねんで!!').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '17:39',
         'time_sec': '1059',
         'song': SongData.get('アジアン セレブレイション').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '21:01',
         'time_sec': '1261',
         'song': SongData.get('アダムとイブのジレンマ').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '26:47',
         'time_sec': '1607',
         'song': SongData.get('ホットチリソース').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '33:36',
         'time_sec': '2016',
         'song': SongData.get('ナージャ!!').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '38:40',
         'time_sec': '2320',
         'song': SongData.get("GOIN'!!!").title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '48:42',
         'time_sec': '2922',
         'song': SongData.get('私がオバさんになっても').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '54:04',
         'time_sec': '3244',
         'song': SongData.get('飾りじゃないのよ涙は').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '1:02:48',
         'time_sec': '3768',
         'song': SongData.get('渚のバルコニー').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '1:06:45',
         'time_sec': '4005',
         'song': SongData.get('青い珊瑚礁').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '1:10:38',
         'time_sec': '4238',
         'song': SongData.get('ロマンスの神様').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '1:15:14',
         'time_sec': '4514',
         'song': SongData.get('そばかす').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '1:49:08',
         'time_sec': '6548',
         'song': SongData.get('Kawaii make MY day!').title,
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       }
     ]
   },
 
   {
-    'title': '',
+    'title': ['', ''],
+    'url': '',
     'date': '',
     'setlist': [
       {
         'time_vis': '',
         'time_sec': '',
         'song': '',
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       },
       {
         'time_vis': '',
         'time_sec': '',
         'song': '',
-        'additional': [[''], ['']]
+        'additional': [[''], ['']],
+        'archive_title': ['', ''],
+        'archive_url': '',
+        'archive_date': ''
       }
     ]
   }
