6ac1016cae2d1992e7babe12fc7a923a283273c2
SisterClaire-Songs <skm9348@naver.com>
2023-05-01 19:44:16 +0900

    # Edit index page info

--- client/views/index_jp.vue
+++ client/views/index_jp.vue
@@ -4,8 +4,13 @@
         現在、2022年12月31日の配信まで更新しました。<br>
         Youtube/bilibiliを合わせて、クレアさんが <b>1001種類</b>の曲を <b>2788回</b> 歌ったデータがあります。<br>
         <br>
-        データが旧バージョンの場合、PCでは「Ctrl+F5」でキャッシュ初期化すれば最新版になります。<br>
-        モバイルの場合、ブラウザのSecret機能を使えば最新版になると思います。<br>
+        <br>
+        申し訳ありませんが、2023年のからは更新が非定期になりました。<br>        
+        以下のYouTubeの再生リストは確認次第更新しているので、リストだけならここを参考にしてください。<br>
+        <a href="https://www.youtube.com/playlist?list=PL5AupjB1xXYKndTwncj7AhCCrCCT76e0B" target="_blank">
+        https://www.youtube.com/playlist?list=PL5AupjB1xXYKndTwncj7AhCCrCCT76e0B</a>
+        <!-- データが旧バージョンの場合、PCでは「Ctrl+F5」でキャッシュ初期化すれば最新版になります。<br> -->
+        <!-- モバイルの場合、ブラウザのSecret機能を使えば最新版になると思います。<br> -->
     </div>
 </template>
 
--- client/views/index_kr.vue
+++ client/views/index_kr.vue
@@ -4,8 +4,12 @@
         현재, 2022년 12월 31일 방송까지 갱신했습니다.<br>
         Youtube/bilibili를 합해서 수녀님이 <b>1001종류</b>의 곡을 <b>2788번</b> 부른 데이터가 있습니다.<br>
         <br>
-        데이터가 구버전일 경우, PC에서는 [Ctrl+F5]로 캐시를 초기화하면 최신버전으로 갱신됩니다.<br>
-        모바일의 경우, 브라우저의 시크릿 기능을 사용하면 최신판 페이지가 표시됩니다.<br>
+        죄송하지만, 2023년부터는 갱신이 비정기가 되었습니다.<br>
+        아래 유튜브 재생목록은 확인하는대로 갱신하고 있으므로, 리스트만이라면 여기를 참고해주시면 감사하겠습니다.<br>
+        <a href="https://www.youtube.com/playlist?list=PL5AupjB1xXYKndTwncj7AhCCrCCT76e0B" target="_blank">
+        https://www.youtube.com/playlist?list=PL5AupjB1xXYKndTwncj7AhCCrCCT76e0B</a>
+        <!-- 데이터가 구버전일 경우, PC에서는 [Ctrl+F5]로 캐시를 초기화하면 최신버전으로 갱신됩니다.<br> -->
+        <!-- 모바일의 경우, 브라우저의 시크릿 기능을 사용하면 최신판 페이지가 표시됩니다.<br> -->
         <br>
         한국어 번역의 경우, 완벽히 맞는 번역이 아닐 수 있는 점 양해 부탁드립니다.
     </div>      
